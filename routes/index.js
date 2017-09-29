e7bd36ed6878d623303a81f53993da3dae8b3807
afshanaman81 <afshan.aman@gmail.com>
2017-09-29 15:41:38 +0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var router = express.Router()
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', { title: 'Find My Way Application' })
+})
+
+router.get('/otherhomes/:homenumber/secondary', function(req, res, next) {
+    var homenum = req.params.homenumber
+
+    // if url parameter :homenum is not a number
+    if (isNaN(homenum)) {
+        // take the visitor to the first home
+        homenum = 1
+    }
+    res.render('index', { title: 'Other Home number ' + homenum })
+})
+
+router.get('/otherhomes/holiday', function(req, res, next) {
+    res.render('index', { title: 'Holiday Home' })
+})
+
+module.exports = router
